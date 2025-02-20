b39770587f7f3a0894204a4760de74a02c815cf4
pedroangelbl <pedroangelbeltranlopez@gmail.com>
2025-02-20 19:59:49 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="shortcut icon" href="{{asset('media/favicon.png')}}" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Eleme | Home</title>
+    @viteReactRefresh
+    @vite(['resources/js/app.js'])
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
